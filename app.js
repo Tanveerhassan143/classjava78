3bacc0e53c85e9aee07df6c0247073a7e1b32a41
unknown <ranatanveerhassan04@gmail.com>
2024-05-29 20:58:47 +0500

    ok

---
+++ app.js
@@ -0,0 +1,25 @@
+// alert("next alert in mathimaticaly opretion used in different way please chack in inspect")
+// var a = 4
+// var b = 3
+// alert(a + b)
+
+// a++
+
+// alert(++a + ++b + ++a + ++b + ++a)
+// a++
+// ++b
+// alert(a + b)
+// alert(++a + ++b + ++b + a++ + ++a)
+
+
+// var a = 4
+// var b = 3
+// console.log(a + b);
+
+// a++
+
+// console.log(++a + ++b + ++a + ++b + ++a);
+// a++
+// ++b
+// console.log(a + b);
+// console.log(++a + ++b + ++b + a++ + ++a);
